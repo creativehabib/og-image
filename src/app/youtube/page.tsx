436430e664eb21c723da76b587bdd61c0fa36a71
creativehabib <shamolrahaman@gmail.com>
2024-09-20 04:08:00 +0600

    error fixed

--- src/app/youtube/page.tsx
+++
@@ -1,9 +0,0 @@
-import YoutubeThumbnailFetcher from "@/components/YoutubeThumbanil";
-
-export default function YoutubeThumbnail(){
-    return(
-        <div>
-            <YoutubeThumbnailFetcher />
-        </div>
-    )
-}
